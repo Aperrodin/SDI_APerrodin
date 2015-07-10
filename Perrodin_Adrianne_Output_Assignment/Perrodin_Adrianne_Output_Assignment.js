fdfa6c690ad8b58bc8521a301d456f1716ad9658
Aperrodin <adrianneperrodin@gmail.com>
2015-07-10 09:49:55 -0500

    Added string

--- Perrodin_Adrianne_Output_Assignment/Perrodin_Adrianne_Output_Assignment.js
+++ Perrodin_Adrianne_Output_Assignment/Perrodin_Adrianne_Output_Assignment.js
@@ -1,3 +1,11 @@
 /**
  * Created by adrianneperrodin on 7/10/15.
  */
+//Adrianne Perrodin
+//SDI Term 02
+//Output Assignment
+
+var jobPosition;
+jobPosition = "Flight Simulator Technician"; //assigning a string
+console.log("My current job I work as a " + jobPosition);
+
